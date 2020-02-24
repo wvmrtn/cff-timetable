d41c944a3571fefdc5b587f684b9ca990ce692e1
wvmrtn <william.vincent.martin@gmail.com>
2020-02-24 15:08:47 +0100

    modifed way arguments are passed

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Created on Thu Dec  5 18:33:40 2019
 
 # import standard libraries
 import datetime
-import math
 import os
 import sys
 import time
@@ -19,16 +18,16 @@ from cff_timetable import CFF, ABSPATH
 
 TEST = False
 if TEST:
-    SCHEDULE = 'test'
-    CHANNEL = 'williammartin'
+    FILENAME = 'schedule_test.csv'
+    CHANNEL = 'general'
 else:
-    SCHEDULE = sys.argv[1]
+    FILENAME = sys.argv[1]
     CHANNEL = sys.argv[2]
 
 if __name__ == '__main__':
     
     # read schedule for checking when to send notifications
-    schedule = pd.read_csv(os.path.join(ABSPATH, 'schedule_{}.csv'.format(SCHEDULE)))
+    schedule = pd.read_csv(os.path.join(ABSPATH, FILENAME))
     # clean schedule
     schedule = schedule[['from', 'to', 'via', 'limit', 'time', 'day', 'notice']]
     
@@ -37,7 +36,7 @@ if __name__ == '__main__':
         # check for day and time
         now = datetime.datetime.now()
         day = now.strftime('%A')
-        if math.isnan(entry['time']):
+        if pd.isna(entry['time']):
             entry_time = now
         else:
             entry_time = datetime.datetime.strptime(entry['time'], '%H:%M')
@@ -48,10 +47,10 @@ if __name__ == '__main__':
             entry_time >= now and \
             (entry_time - now).seconds < entry['notice']:
             
-            cff = CFF(level = 'INFO', notify_slack = True, channel = '#cff_{}'.format(CHANNEL))
+            cff = CFF(level = 'INFO', notify_slack = True, channel = '#{}'.format(CHANNEL))
             connections = cff.returnConnections(entry['from'], 
                                                 entry['to'],
-                                                via = None if math.isnan(entry['via']) else entry['via'],
+                                                via = None if pd.isna(entry['via']) else entry['via'],
                                                 limit = entry['limit'],
                                                 time = entry['time'])
             
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 source ~/python_env/cff/bin/activate
 
 # run python script
-python ~/Github/cff-timetable/main.py williammartin williammartin &> ~/Github/cff-timetable/run_williammartin.log
+python ~/Github/cff-timetable/main.py schedule_williammartin.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
 
