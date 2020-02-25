617309671c5a2c3c85bdddab2d898eed9aee96bd
William Martin <william.vincent.martin@gmail.com>
2020-02-25 21:20:56 +0100

    changed ruN

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 source ~/python_env/cff/bin/activate
 
 # run python script
-#python ~/Github/cff-timetable/main.py schedule_williammartin.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
-#python ~/Github/cff-timetable/main.py schedule_nelsonkoch.csv cff_nelsonkoch &> ~/Github/cff-timetable/run_nelsonkoch.log
-python ~/Github/cff-timetable/main.py schedule_test.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
+python ~/Github/cff-timetable/main.py schedule_williammartin.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
+python ~/Github/cff-timetable/main.py schedule_nelsonkoch.csv cff_nelsonkoch &> ~/Github/cff-timetable/run_nelsonkoch.log
+#python ~/Github/cff-timetable/main.py schedule_test.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
 
