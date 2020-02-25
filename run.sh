7ac2c3f703b6da31daa874ff9c90233a37331643
William Martin <william.vincent.martin@gmail.com>
2020-02-25 17:43:20 +0100

    added line for nelson in run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ source ~/python_env/cff/bin/activate
 
 # run python script
 python ~/Github/cff-timetable/main.py schedule_williammartin.csv cff_williammartin &> ~/Github/cff-timetable/run_williammartin.log
+#python ~/Github/cff-timetable/main.py schedule_nelsonkoch.csv cff_nelsonkoch &> ~/Github/cff-timetable/run_nelsonkoch.log
 
